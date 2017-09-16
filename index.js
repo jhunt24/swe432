86b52e2f254faba1b0bf88922a222bab4a13d88b
John <hunt2882@yahoo.com>
2017-09-15 22:59:50 -0400

    hw2

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(express.static(__dirname + '/public'));
+class Beer {//class for the sake of 2 class requirement, the api offers more data if needed
+    constructor(name, firstBrewed, abv, ibu) {
+        this.name = name;
+        this.firstBrewed = firstBrewed;
+        this.abv = abv;
+        this.ibu = ibu;
+    }
+}
+let beerArray = [];//store Beer class here
+
+let fetch = require('node-fetch');
+
+fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')//api seems to limit 80 per call,
+// can do a 2nd fetch if it needs to be larger
+    .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+        for(let i in json){
+            beerArray[i] = new Beer(json[i].name, json[i].first_brewed, json[i].abv, json[i].ibu);
+        }
+});
 
 app.get('/', function(request, response) {
-    response.send("helloooo world")
+    response.send(beerArray)
 });
 
 app.listen(app.get('port'), function() {
