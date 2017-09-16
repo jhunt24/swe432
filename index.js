1362658c28f484540d79aca37464b51a878ff405
Nolan Meeks <nolanm4509@gmail.com>
2017-09-16 18:54:15 -0400

    Get beer by ID

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('port', (process.env.PORT || 5000));
 2. Get or update the abv content of a beer
     GET, PUT /beer/:beerId/abv
 
-3. Get the list of beers that are strong (abv > 6.0)
+3. Get the list of beers that are strong (abv > 7.0)
     GET /strongBeers
 
 4. Get a list of beers that are of an ibu greater than (ibu_gt X)
@@ -22,8 +22,9 @@ app.set('port', (process.env.PORT || 5000));
  */
 
 class Beer {//class for the sake of 2 class requirement, the api offers more data if needed
-    constructor(name, firstBrewed, abv, ibu) {
+    constructor(name, id, firstBrewed, abv, ibu) {
         this.name = name;
+        this.id = id;
         this.firstBrewed = firstBrewed;
         this.abv = abv;
         this.ibu = ibu;
@@ -39,12 +40,25 @@ fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')//api seems to limit
         return res.json();
     }).then(function(json) {
         for(let i in json){
-            beerArray[i] = new Beer(json[i].name, json[i].first_brewed, json[i].abv, json[i].ibu);
+            beerArray[i] = new Beer(json[i].name, json[i].id, json[i].first_brewed, json[i].abv, json[i].ibu);
         }
 });
 
 app.get('/', function(request, response) {
     response.send(beerArray)
+});
+
+app.get('/beer/:beerId', function(request, response){
+    let beerId = Number(request.params.beerId);
+    let beerToReturn = null;
+
+    beerArray.map(function(beer) {
+        if(beer.id === beerId){
+            beerToReturn = beer;
+        }
+    });
+
+    response.send({beer: beerToReturn});
 });
 
 app.listen(app.get('port'), function() {
