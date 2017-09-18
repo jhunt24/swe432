e9659ad84851eb68ac3ec350863b315a61adcff8
Nolan Meeks <nolanm4509@gmail.com>
2017-09-18 16:52:23 -0400

    All Gets complete + messages + hop class

--- index.js
+++ index.js
@@ -14,16 +14,26 @@ app.set('port', (process.env.PORT || 5000));
     GET /strongBeers
 4. Get a list of beers that are of an ibu greater than (ibu_gt X)
     GET /beer/ibu_gt/:ibu
-5. Get a list of beers that match the supplied name
+5. Get a list of beers that include the specified hop searched for
+    GET /beer/hops/:hopName
+6. Get a list of beers that match the supplied name
     GET /beer/name/:beerName
  */
 
+class Hop {
+    constructor(name, attribute) {
+        this.name = name;
+        this.attribute = attribute;
+    }
+}
+
 class Beer {//class for the sake of 2 class requirement, the api offers more data if needed
-    constructor(name, id, abv, ibu) {
+    constructor(name, id, abv, ibu, hops) {
         this.name = name;
         this.id = id - 1;//so array and id numbers match
         this.abv = abv;
         this.ibu = ibu;
+        this.hops =  hops;
     }
 }
 let beerArray = [];//store Beer class here
@@ -36,7 +46,11 @@ fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')//api seems to limit
         return res.json();
     }).then(function(json) {
     for(let i in json){
-        beerArray[i] = new Beer(json[i].name, json[i].id, json[i].abv, json[i].ibu);
+        let newHops = [];
+        for(let h in json[i].ingredients.hops){
+            newHops[h] = new Hop(json[i].ingredients.hops[h].name, json[i].ingredients.hops[h].attribute);
+        }
+        beerArray[i] = new Beer(json[i].name, json[i].id, json[i].abv, json[i].ibu, newHops);
     }
 });
 
@@ -118,7 +132,7 @@ app.get('/beer/:beerId/abv', function(request, response){
         response.send("ID out of range");
         return;
     }
-    response.send({abv: abvToReturn.abv});
+    response.send({abv: abvToReturn.abv});//{"abv":4.5}
 });
 
 app.put('/beer/:beerId/abv', function(request, response){
@@ -137,20 +151,54 @@ app.put('/beer/:beerId/abv', function(request, response){
 //Scenario 3
 app.get('/strongBeers', function(request, response){
     var strongBeerArray = beerArray.filter((beer) => beer.abv >= 7.0);
+    if(strongBeerArray.length === 0) {
+        response.send("There are no beers that have an ABV of 7.0 or greater.");
+        return;
+    }
     response.send(strongBeerArray);
 });
 
 //Scenario 4
 app.get('/beer/ibu_gt/:ibu', function(request, response){
     let beerIbu = Number(request.params.ibu);
-    var newBeerArray = beerArray.filter(beer => beer.ibu >= beerIbu);
+    var newBeerArray = beerArray.filter(beer => beer.ibu > beerIbu);
+    if(newBeerArray.length === 0) {
+        response.send("There are no beers with an IBU greater than " + beerIbu + ".");
+        return;
+    }
     response.send(newBeerArray);
 });
 
 //Scenario 5
+app.get('/beer/hops/:hopName', function(request, response){
+    let beerHop = request.params.hopName;
+    var newBeerArray = [];
+    var ctr = 0;
+    for(let beer in beerArray) {
+        for(let i in beerArray[beer].hops) {
+            if(beerArray[beer].hops[i].name.includes(beerHop)){
+                newBeerArray[ctr] = beerArray[beer];
+                ctr++;
+                break;
+            }
+        }
+    }
+        
+    if(newBeerArray.length === 0) {
+        response.send("There are no beers that contain hops that contain '" + beerHop + "' in their name. (CASE SENSITIVE)");
+        return;
+    }
+    response.send(newBeerArray);
+})
+
+//Scenario 6 (extra)
 app.get('/beer/name/:beerName', function(request, response){
     let beerName = request.params.beerName;
     var newBeerArray = beerArray.filter(beer => beer.name.includes(beerName));
+    if(newBeerArray.length === 0){
+        response.send("There are no beers that contain '" + beerName + "' in their name. (CASE SENSITIVE)");
+        return;
+    }
     response.send(newBeerArray);
 });
 
